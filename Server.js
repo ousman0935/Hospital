86d9b8f4b5f5137e98b0f92c2a096ed5e6a3ce19
Ousman0935 <ousman0935@gmail.com>
2026-02-06 15:25:12 +0300

    server cors

--- Server.js
+++ Server.js
@@ -14,7 +14,8 @@ dotenv.config();
 dbConnect();
 const allowedOrigins = [
   'http://localhost:3000',
- "https://hospital-1-q3bj.onrender.com/"
+ "https://hospital-1-q3bj.onrender.com/",
+  "https://hospital-1-q3bj.onrender.com"
  
 ];
 const app=express();
