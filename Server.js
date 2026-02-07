a9e709b3d45eed0f7be89589d8275f35ac187692
Ousman0935 <ousman0935@gmail.com>
2026-02-07 09:01:20 +0300

    server origion

--- Server.js
+++ Server.js
@@ -14,7 +14,7 @@ dotenv.config();
 dbConnect();
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://hospital-1-q3bj.onrender.com'
+  'https://hospital-frontend-64xi.onrender.com/'
 ];
 
 const app=express();
